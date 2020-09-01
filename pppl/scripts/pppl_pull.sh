f62551a8ba2651774ed89304f3b9284cb9aaf3c7
James R. Griffin III <jrgriffiniii@gmail.com>
2020-09-01 09:56:14 -0400

    Avoiding deleting the logs for the AWS S3 transfer

--- pppl/scripts/pppl_pull.sh
+++ pppl/scripts/pppl_pull.sh
@@ -7,13 +7,13 @@ export DSPACE_AWS_S3=$DSPACE_PPPL_HOME/s3
 export AWS_BUCKET=pppldataspace
 export DSPACE_EPERSON="pppldataspace@princeton.edu"
 
+export DSPACE_PPPL_EMAIL="jrg5@princeton.edu"
 export LOG_LEVEL=DEBUG
-export ERROR_EMAIL_TO="jrg5@princeton.edu"
 
 source $DSPACE_PPPL_HOME/scripts/runit.sh
 if [ "$?" != 0 ]; then
   echo 'fail'
-  mutt -s 'dataspace import of pppl package failed' $ERROR_EMAIL_TO  <<- EOM
+  mutt -s 'dataspace import of pppl package failed' $DSPACE_PPPL_EMAIL  <<- EOM
     script ran on $(hostname)
     see dated log file in s3://$AWS_BUCKET/log
 EOM
--- pppl/scripts/runit.sh
+++ pppl/scripts/runit.sh
@@ -28,10 +28,12 @@ export IMPORT_LOG_FILE=$IMPORT_LOG_DIR/$LONG_DATE-s3-bucket.log
 mkdir -p $IMPORT_LOG_DIR
 touch $IMPORT_LOG_FILE
 
+# This synchronizes from S3 to the local file system
 echo  "Logging to $IMPORT_LOG_FILE"
 echo "aws s3 sync s3://$AWS_BUCKET $DSPACE_AWS_S3 >> $IMPORT_LOG_FILE"
 aws s3 sync s3://$AWS_BUCKET $DSPACE_AWS_S3 >> $IMPORT_LOG_FILE
 
+# This uploads any new SIPs
 fgrep 'Download:' $IMPORT_LOG_FILE >> /dev/null
 if [ $? == 0 ]; then
   echo "python $DATASPACE_IMPORT $DSPACE_AWS_S3 $DSPACE_EPERSON >> $IMPORT_LOG_FILE"
@@ -43,12 +45,12 @@ if [[ $status -eq 0 ]]; then
   echo "FAILURE to sync s3 bucket" >> $IMPORT_LOG_FILE
 fi
 
+# This synchronizes from the local file system to S3
 echo "aws s3 sync $DSPACE_AWS_S3/imports s3://$AWS_BUCKET/imports >> $IMPORT_LOG_FILE"
 aws s3 sync $DSPACE_AWS_S3/imports s3://$AWS_BUCKET/imports >> $IMPORT_LOG_FILE
 
 if [ -e $IMPORT_LOG_FILE ]; then
-  echo "$IMPORT_LOG_FILE is empty - deleting"
-  rm $IMPORT_LOG_FILE
+  echo "Warning: $IMPORT_LOG_FILE is empty"
 else
   echo "aws s3 sync $DSPACE_AWS_S3/log s3://$AWS_BUCKET/log"
   aws s3 sync $DSPACE_AWS_S3/log s3://$AWS_BUCKET/log
